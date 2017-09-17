eb14ffdcf12e550c7d43e73c7f9a7664076a5f05
Venantas <30750383+Venantas@users.noreply.github.com>
2017-09-17 22:24:31 +0300

    Update functions.php

--- functions/functions.php
+++ functions/functions.php
@@ -106,7 +106,7 @@ echo $total;
 //Categories
 function getCats(){
   global $con;
-$get_cats = "select * from Categories";
+$get_cats = "select * from categories";
   $run_cats = mysqli_query($con, $get_cats);
   while ($row_cats=mysqli_fetch_array($run_cats)){
 
